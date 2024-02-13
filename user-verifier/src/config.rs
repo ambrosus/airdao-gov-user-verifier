6ba1da350d2d6a314eacacaa520fd85519c93ef4
ZelionD <k.kotliar@inc4.net>
2024-02-13 09:13:37 -0500

    remove user_db from appconfig

--- user-verifier/src/config.rs
+++ user-verifier/src/config.rs
@@ -1,7 +1,6 @@
 use serde::Deserialize;
 
 use crate::{fractal::FractalConfig, signer::SignerConfig};
-use shared::common::UserDbConfig;
 
 #[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -9,5 +8,4 @@ pub struct AppConfig {
     pub listen_address: String,
     pub fractal: FractalConfig,
     pub signer: SignerConfig,
-    pub user_db: UserDbConfig,
 }
